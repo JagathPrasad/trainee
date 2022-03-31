2564506232e1b982d87d4d4c61c0c702372ffaf3
Jagath Karra <jagathprasad0@gmail.com>
2022-03-31 20:11:36 +0530

    udpated

--- client_app/src/App.js
+++ client_app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navarbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
 
 
   return (
-    <>
+    <div className='App'>
       <Navarbar />
-    </>
+      <Footer />
+    </div >
 
   );
 }
