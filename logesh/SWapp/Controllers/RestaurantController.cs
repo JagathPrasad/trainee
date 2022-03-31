c265107525e9aa821d4e66f4e93cd058cd465d92
LogeshBabuLB <logeshbabu9924@gmail.com>
2022-03-31 20:34:35 +0530

    updated source code

--- logesh/SWapp/Controllers/RestaurantController.cs
+++ logesh/SWapp/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using SWapp.Data;
 using SWapp.Model;
 
@@ -6,6 +7,7 @@ namespace SWapp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+  
     public class RestaurantController : Controller
     {
         
