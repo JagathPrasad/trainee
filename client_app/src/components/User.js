922553504c1f1361a91539dce54ec71b8893fa01
Chidanu <anusha.nataraju.m.p@gmail.com>
2022-04-19 20:10:55 +0530

    delivery page view finction and user page css

--- client_app/src/components/User.js
+++ client_app/src/components/User.js
@@ -28,8 +28,8 @@ const User = () => {
       <Navbar />
       <Sidebar />
 
-      <div class=" container mx-auto px-4 sm:px-8">
-        <div class="py-8">
+      <div class=" container mx-auto px-4 sm:px-8 ">
+        <div class="py-5">
           <div>
             <h2 class="text-2xl font-semibold leading-tight text-center text-blue-900">USERS</h2>
           </div>
@@ -37,31 +37,31 @@ const User = () => {
             <div
               class="inline-block min-w-full shadow-md rounded-lg overflow-hidden"
             >
-              <table class="min-w-full leading-normal">
+              <table class="min-w-full leading-normal " >
                 <thead>
                   <tr>
-                    <th
+                    <th 
                       class="px-15 py-6 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
                     >
                       USER NAME
                     </th>
                     <th
-                      class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
+                      class="px-3 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
                     >
                       MOBILE NO
                     </th>
                     <th
-                      class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
+                      class="px-3 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
                     >
                       USER ID
                     </th>
                     <th
-                      class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
+                      class="px-3 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xm font-semibold text-blue-700 uppercase tracking-wider"
                     >
                       Status
                     </th>
                     <th
-                      class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"
+                      class="px-3 py-3 border-b-2 border-gray-200 bg-gray-100"
                     ></th>
                   </tr>
                 </thead>
@@ -71,7 +71,7 @@ const User = () => {
                     <tr key={index}>
                       
 
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td class="px-2 py-2 border-b border-gray-200 bg-white text-sm">
                         <div class="flex">
                           <div class="flex-shrink-0 w-10 h-10">
                             <img
@@ -88,16 +88,16 @@ const User = () => {
                           </div>
                         </div>
                       </td>
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td class="px-3 py-5 border-b border-gray-200 bg-white text-sm">
                         <p class="text-gray-900 whitespace-no-wrap">{x.mobile_no}</p>
                         {/* <p class="text-gray-600 whitespace-no-wrap">INR</p> */}
                       </td>
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td class="px-3 py-5 border-b border-gray-200 bg-white text-sm">
                         <p class="text-gray-900 whitespace-no-wrap">{x.user_id}</p>
                         {/* <p class="text-gray-600 whitespace-no-wrap">Due in 3 days</p> */}
                       </td>
 
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                      <td class="px-3 py-5 border-b border-gray-200 bg-white text-sm">
                         <span
                           class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
                         >
@@ -108,23 +108,7 @@ const User = () => {
                           <span class="relative">Paid</span>
                         </span>
                       </td>
-                      <td
-                        class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right"
-                      >
-                        <button
-                          type="button"
-                          class="inline-block text-gray-500 hover:text-gray-700"
-                        >
-                          <svg
-                            class="inline-block h-6 w-6 fill-current"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z"
-                            />
-                          </svg>
-                        </button>
-                      </td>
+                      
                     </tr>
                      
                   );})}
