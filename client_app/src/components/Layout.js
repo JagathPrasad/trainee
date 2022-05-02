1bf0fdb084b266189d927d86632a389cde6beb2e
Chidanu <anusha.nataraju.m.p@gmail.com>
2022-05-02 09:46:59 +0530

    small change in layout page

--- client_app/src/components/Layout.js
+++ client_app/src/components/Layout.js
@@ -7,12 +7,19 @@ import React, { useState, useContext } from 'react';
 const Layout = (props) => {
     const { state } = useContext(store);
     console.log('asdfasdfsasfafdsafdsadfasdfasdfa', state.user);
+    //read from session storage
     const page = props.page;
     console.log('page', page);
-    const renderPage = () => {
-        return <div><Navbar />
-            <Sidebar /></div>;
-    }
+    useEffect(() => {
+        const loggedInUser = sessionStorage.getItem('user_details');
+
+        console.log('loggedInUser', loggedInUser);
+        if (loggedInUser != null) {
+           // goHome();
+        }
+    }, []);
+
+    
     return (
         <div>
 
