53f65fe4bf75fd7e155bf733462703553cb50d2a
Chidanu <anusha.nataraju.m.p@gmail.com>
2022-05-02 20:26:19 +0530

    side bar and layout page fixed the issues after refresh the page

--- client_app/src/components/Layout.js
+++ client_app/src/components/Layout.js
@@ -2,24 +2,30 @@ import Footer from './Footer'
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import { store } from '../store/user';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext,useEffect } from 'react';
 
 const Layout = (props) => {
-    const { state } = useContext(store);
+    
+    const { state,dispatch } = useContext(store);
+
     console.log('asdfasdfsasfafdsafdsadfasdfasdfa', state.user);
     //read from session storage
     const page = props.page;
     console.log('page', page);
+    // const renderPage = () => {
+    //     return <div><Navbar />
+    //         <Sidebar /></div>;
+    // }
+
     useEffect(() => {
-        const loggedInUser = sessionStorage.getItem('user_details');
+        const loggedInUser = JSON.parse( sessionStorage.getItem('user_details'));
 
         console.log('loggedInUser', loggedInUser);
         if (loggedInUser != null) {
+            dispatch({ type: 'ADD_USER', payload: { user: loggedInUser } });
            // goHome();
         }
     }, []);
-
-    
     return (
         <div>
 
