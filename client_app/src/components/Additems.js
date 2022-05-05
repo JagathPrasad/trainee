d40964902ec8958102bf48a18332878418e521ee
LogeshBabuLB <logeshbabu9924@gmail.com>
2022-05-05 09:47:44 +0530

    post method on add items page

--- client_app/src/components/Additems.js
+++ client_app/src/components/Additems.js
@@ -6,7 +6,7 @@ import { baseUrl } from './utility/api_config';
 
 const AddItems = () => {
 
-    //  console.log('baseUrl', baseUrl);
+  //  console.log('baseUrl', baseUrl);
     const { Add, handleSubmit } = useForm();
     //     const [item_details, setItemDetails] = useState([]);
     //     const Setitems = () => {
@@ -26,8 +26,8 @@ const AddItems = () => {
     const [Discription, setDiscription] = useState("");
     const [Price, setPrice] = useState("");
     const [Serving, setServing] = useState("");
-    const [isveg, setisveg] = useState("");
-    const [Coupon, setCoupon] = useState("");
+    const [isveg,setisveg] = useState("");
+    const [Coupon,setCoupon] = useState("");
     const [CouponPercentage, setCouponPercentage] = useState("");
     const [isPremium, setisPremium] = useState("");
     const [CategoryId, setCategoryId] = useState("");
@@ -51,17 +51,17 @@ const AddItems = () => {
         reader.readAsDataURL(file);
         reader.onload = () => {
             base64 = reader.result;
-            console.log('base64', base64);
+              console.log('base64', base64);
             setImage64(base64);
         };
         reader.onerror = (error) => {
             console.log('error :', error);
         };
     };
-    // console.log('name',name);
+  // console.log('name',name);
     //baseimg(selectedfile[0])
     const onSubmit = (data, e) => {
-        // console.log('data',data);
+       // console.log('data',data);
         let item = {
             id: '00000000-0000-0000-0000-000000000000',
             name: name,
@@ -76,9 +76,9 @@ const AddItems = () => {
             is_premium: isPremium,
             category_id: CategoryId,
         };
-        console.log("item", item);
-        axios.post(baseUrl + 'item/postadditem', item).then((res) => {
-            console.log('item added', res.data);
+       console.log("item", item);
+        axios.post(baseUrl+'item/postadditem', item).then((res) => {
+console.log('item added',res.data);
         }).catch(() => {
 
         })
@@ -89,9 +89,9 @@ const AddItems = () => {
     const refreshPage = () => {
         window.location.reload();
     }
-    const list = () => {
-        console.log('name :', name, 'caption :', caption, 'Price :', Price, 'Coupon :', Coupon, image64);
-    }
+const list =()=>{
+    console.log('name :',name,'caption :',caption,'Price :',Price,'Coupon :',Coupon,image64);
+}
 
     return (
         <div class="  overflow-hidden sm:rounded-lg  additems">
@@ -108,22 +108,22 @@ const AddItems = () => {
                                     <div class="grid grid-cols-6 gap-6">
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for="name" class="block text-sm  font-medium border-black text-gray-700">Food Name</label>
-                                            <input type="text" name="name" id="name" Value={name} onChange={(e) => setName(e.target.value)} class=" mt-1 block w-6/12  py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
+                                            <input type="text" name="name" id="name" Value={name} onChange={(e)=>setName(e.target.value)} class=" mt-1 block w-6/12  py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
                                         </div>
 
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for="caption" class="block text-sm font-medium text-gray-700"> Caption</label>
-                                            <input type="text" name="caption" id="caption" Value={caption} onChange={(e) => setCaption(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
+                                            <input type="text" name="caption" id="caption"Value={caption} onChange={(e)=>setCaption(e.target.value)}  class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
                                         </div>
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for="description" class="block text-sm font-medium text-gray-700"> Discription</label>
-                                            <input type="text" name="description" id="description" Value={Discription} onChange={(e) => setDiscription(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
+                                            <input type="text" name="description" id="description"Value={Discription} onChange={(e)=>setDiscription(e.target.value)}  class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
                                         </div>
 
 
                                         <div class="col-span-6 sm:col-span-3">
                                             <label for="amount" class="block text-sm font-medium text-gray-700"> Price</label>
-                                            <input type="text" name="amount" id="amount" autocomplete="amount" Value={Price} onChange={(e) => setPrice(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
+                                            <input type="text" name="amount" id="amount" autocomplete="amount"Value={Price} onChange={(e)=>setPrice(e.target.value)}  class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  sm:text-sm" />
                                         </div>
 
 
@@ -136,11 +136,11 @@ const AddItems = () => {
 
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for="serving" class="block text-sm font-medium text-gray-700">Serving</label>
-                                            <input type="text" name="serving" id="serving" autocomplete="serving" Value={Serving} onChange={(e) => setServing(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                            <input type="text" name="serving" id="serving" autocomplete="serving" Value={Serving} onChange={(e)=>setServing(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         </div>
                                         <div class="col-span-6 sm:col-span-3">
                                             <label for="isveg" class="block text-sm font-medium text-gray-700">VEG</label>
-                                            <select id="isveg" name="isveg" autocomplete="isveg" Value={isveg} onChange={(e) => setisveg(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                            <select id="isveg" name="isveg" autocomplete="isveg"Value={isveg} onChange={(e)=>setisveg(e.target.value)}  class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                                 <option>True</option>
                                                 <option>False</option>
 
@@ -148,15 +148,15 @@ const AddItems = () => {
                                         </div>
                                         <div class="col-span-6 sm:col-span-3">
                                             <label for="coupon" class="block text-sm font-medium text-gray-700"> Coupon</label>
-                                            <input type="text" name="coupon" id="coupon" autocomplete="coupon" Value={Coupon} onChange={(e) => setCoupon(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                            <input type="text" name="coupon" id="coupon" autocomplete="coupon" Value={Coupon} onChange={(e)=>setCoupon(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         </div>
                                         <div class="col-span-6 sm:col-span-3">
                                             <label for="coupon_percentage" class="block text-sm font-medium text-gray-700"> Coupon Percentage</label>
-                                            <input type="text" name="coupon_percentage" id="coupon_percentage" Value={CouponPercentage} onChange={(e) => setCouponPercentage(e.target.value)} autocomplete="coupon_percentage" class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                            <input type="text" name="coupon_percentage" id="coupon_percentage"Value={CouponPercentage} onChange={(e)=>setCouponPercentage(e.target.value)} autocomplete="coupon_percentage" class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         </div>
                                         <div class="col-span-6 sm:col-span-3">
                                             <label for="is_premium" class="block text-sm font-medium text-gray-700">Premium</label>
-                                            <select id="is_premium" name="is_premium" autocomplete="is_premium" Value={isPremium} onChange={(e) => setisPremium(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                            <select id="is_premium" name="is_premium" autocomplete="is_premium" Value={isPremium} onChange={(e)=>setisPremium(e.target.value)}class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                                 <option>True</option>
                                                 <option>False</option>
 
@@ -164,17 +164,17 @@ const AddItems = () => {
                                         </div>
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for=" category_id" class="block text-sm font-medium text-gray-700"> Category Id</label>
-                                            <input type="number" name=" category_id" id=" category_id" autocomplete=" category_id" Value={CategoryId} onChange={(e) => setCategoryId(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                            <input type="number" name=" category_id" id=" category_id" autocomplete=" category_id" Value={CategoryId} onChange={(e)=>setCategoryId(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         </div>
 
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for=" vendorprepartiontime" class="block text-sm font-medium text-gray-700"> Waiting</label>
-                                            <input type="number" name=" vendorprepartiontime" id=" vendorprepartiontime" autocomplete=" vendorprepartiontime" Value={Waiting} onChange={(e) => setWaiting(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                            <input type="number" name=" vendorprepartiontime" id=" vendorprepartiontime" autocomplete=" vendorprepartiontime"  Value={Waiting} onChange={(e)=>setWaiting(e.target.value)}class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         </div>
 
                                         <div class="col-span-1 sm:col-span-3">
                                             <label for="city" class="block text-sm font-medium text-gray-700">Slots</label>
-                                            <input type="text" name="city" id="city" autocomplete="address-level2" Value={Slots} onChange={(e) => setSlots(e.target.value)} class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                            <input type="text" name="city" id="city" autocomplete="address-level2"  Value={Slots} onChange={(e)=>setSlots(e.target.value)}class="mt-1 block w-6/12 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         </div>
 
 
@@ -183,7 +183,7 @@ const AddItems = () => {
                                     </div>
                                 </div>
                                 <div class="px-4 py-3 bg-gray-300 text-right sm:px-6">
-                                    <button onClick={list} type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                                    <button  onClick={list} type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
                                     <button onClick={refreshPage} class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >Clear</button>
 
                                 </div>
