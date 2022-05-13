6d40aa1fe0ffbb5d93722a79f29f83be4b202974
mangai murugan <mmangaimurugan@gmail.com>
2022-05-13 18:49:49 +0530

    updating media queries

--- client_app/src/components/Itemdetails.js
+++ client_app/src/components/Itemdetails.js
@@ -15,6 +15,7 @@ const Itemdetails = () => {
   const [user_type, setUserType] = useState('');
   const [showpopup, setPopup] = useState(false);
   const [Item_delete, setItem_delete] = useState([]);
+  const [currentid, setCurrentId] = useState([]);
   
  
  
@@ -40,35 +41,52 @@ const Itemdetails = () => {
     });
   }  
 
-  const Delete = (vendorid) => {
-    axios.delete(baseUrl+'/item/deletevendoritem/'+ vendorid).then((res) => {
-      console.log(res.data, 'success');
-      //setTimeout(1000)      
-      //setItemDetails(res.data);
-      if(res.data == true)
-      {
-        Getvendoritem();
-      }
-      console.log('Item_delete', Item_delete);
-    }).catch((error) => {
-      console.log(error, 'success');
-    });
-  }
+  // const Delete = (id) => {
+  //   axios.delete(baseUrl+'/item/deletevendoritem/'+ id).then((res) => {
+  //     console.log(res.data, 'success');
+  //     //setTimeout(1000)      
+  //     //setItemDetails(res.data);
+  //     if(res.data == true)
+  //     {
+  //       Getvendoritem();
+  //     }
+  //     console.log('Item_delete', Item_delete);
+  //   }).catch((error) => {
+  //     console.log(error, 'success');
+  //   });
+  // }
 
   useEffect(() => {
     GetUsers();  
     
   }, []);
 
+  const Delete = (id) => {
+    //Getusers();
+    //Getdelete(user_id);
+    axios.delete(baseUrl + '/item/deletevendoritem/' + id).then((res) => {
+      console.log(res.data, 'success');
+      if (res.data === true) {
+        GetUsers();
+      }
+      //setUserDelete(res.data);
+      console.log('Item_delete', Item_delete);
+    }).catch((error) => {
+      console.log(error, 'success');
+    });
+  }
+
     
   const ShowDetails = (data, type) => {
+    
+    
+     setCurrentId(data.id);
     if (type == 'itemslist') {
       Getvendoritem(data, type);
     } else {
       console.log('data', data);
       setDetails(data);
-      setUserType(type);
-
+     setUserType(type);
     }
 
   }
@@ -363,14 +381,14 @@ const Itemdetails = () => {
     </div>;
   }
 
-  const RenderDelete = (vendorid) => {
+  const RenderDelete = () => {
     confirmAlert({
       title: 'Confirm to submit',
       message: 'Are you sure to do this.',
       buttons: [
         {
           label: 'Yes',
-          onClick: () => Delete(vendorid)
+          onClick: () => Delete(currentid)
         },
         {
           label: 'No',
